0cd9155a69859e119beabced97cad60eefccda16
noah <781172480@qq.com>
2025-05-30 11:24:28 +0800

    a2a更新为Runnable接口

--- src/main/agents/a2a/Agent2Agent.ts
+++ src/main/agents/a2a/Agent2Agent.ts
@@ -18,22 +18,32 @@ import { ChatOptions } from '@/entity/Chat';
 import { Agent } from '@/entity/Agent';
 import * as fs from 'fs';
 import path from 'path';
+import { BaseLangChainParams } from '@langchain/core/language_models/base';
+import { IterableReadableStream } from '@langchain/core/utils/stream';
 
-export const createA2A = async (
-  agent: Agent,
-  params: {
-    store?: BaseStore;
-    model?: BaseChatModel;
-    messageEvent?: AgentMessageEvent;
-    chatOptions?: ChatOptions;
-    signal?: AbortSignal;
-    configurable?: Record<string, any>;
-  },
-) => {
-  const client = new A2AClient(agent.remote_url, fetch);
-  const agentCard = await new A2ACardResolver(agent.remote_url).getAgentCard();
+class A2AChain extends Runnable<any, any, RunnableConfig> {
+  //implements BaseLangChainParams
 
-  const add = async (input: any, config: RunnableConfig) => {
+  client: A2AClient;
+
+  agentCard: AgentCard;
+
+  messageEvent?: AgentMessageEvent;
+
+  constructor(params: {
+    client: A2AClient;
+    agentCard: AgentCard;
+    messageEvent: AgentMessageEvent;
+  }) {
+    super();
+    this.client = params.client;
+    this.agentCard = params.agentCard;
+    this.messageEvent = params?.messageEvent;
+  }
+
+  lc_namespace: string[] = ['a2a'];
+
+  async invoke(input: any, config?: RunnableConfig): Promise<AIMessage> {
     const { messages } = input;
     const chatId = config?.configurable?.chatId;
     const workspace = config?.configurable?.workspace;
@@ -46,10 +56,10 @@ export const createA2A = async (
     }
     const aiMessage = new AIMessage('');
     aiMessage.id = uuidv4();
-    aiMessage.name = agentCard.name;
-    await params?.messageEvent?.created([aiMessage]);
+    aiMessage.name = this.agentCard.name;
+    await this.messageEvent?.created([aiMessage]);
     try {
-      const task = await client.sendTask({
+      const task = await this.client.sendTask({
         id: uuidv4(),
         sessionId: chatId,
         message: {
@@ -78,13 +88,96 @@ export const createA2A = async (
         }
       }
       aiMessage.content = output_content;
-      await params?.messageEvent?.finished([aiMessage]);
+      await this.messageEvent?.finished([aiMessage]);
     } catch (err) {
       aiMessage.content = '';
       aiMessage.additional_kwargs.error = err.message;
-      await params?.messageEvent?.finished([aiMessage]);
-      // return new AIMessage(err.message);
+      await this.messageEvent?.finished([aiMessage]);
     }
-  };
-  return RunnableLambda.from(add);
+    return aiMessage;
+  }
+
+  async stream(
+    input: any,
+    config?: RunnableConfig,
+  ): Promise<IterableReadableStream<AIMessage>> {
+    const that = this;
+    async function* generateStream() {
+      const aiMessage = await that.invoke(input, config);
+      yield aiMessage;
+    }
+    const stream = IterableReadableStream.fromAsyncGenerator(generateStream());
+
+    return stream;
+    // const { messages } = input;
+    // const chatId = config?.configurable?.chatId;
+    // const workspace = config?.configurable?.workspace;
+    // const lastMessage: BaseMessage = messages[messages.length - 1];
+    // let role;
+    // let content;
+    // if (isHumanMessage(lastMessage)) {
+    //   role = 'user';
+    //   content = lastMessage.text;
+    // }
+    // const aiMessage = new AIMessage('');
+    // aiMessage.id = uuidv4();
+    // aiMessage.name = this.agentCard.name;
+    // await this.messageEvent?.created([aiMessage]);
+    // try {
+    //   const task = await this.client.sendTaskSubscribe({
+    //     id: uuidv4(),
+    //     sessionId: chatId,
+    //     message: {
+    //       role: role as Role,
+    //       parts: [{ type: 'text', text: content }],
+    //     },
+    //   });
+    //   for await (const chunk of task) {
+    //     console.log(chunk);
+    //   }
+    //   // if (task.artifacts && task.artifacts.length > 0) {
+    //   //   for (const artifact of task.artifacts) {
+    //   //     if (artifact.name) {
+    //   //       const text = artifact.parts[0].text;
+    //   //       if (text) {
+    //   //         const filePath = path.join(workspace, artifact.name);
+    //   //         fs.writeFileSync(filePath, text);
+    //   //         output_content += `\n<file>${filePath}</file>`;
+    //   //       }
+    //   //     }
+    //   //   }
+    //   // }
+    //   // aiMessage.content = output_content;
+    //   debugger;
+    //   await this.messageEvent?.finished([aiMessage]);
+    // } catch (err) {
+    //   aiMessage.content = '';
+    //   aiMessage.additional_kwargs.error = err.message;
+    //   await this.messageEvent?.finished([aiMessage]);
+    //   // return new AIMessage(err.message);
+    // }
+    // return null;
+  }
+}
+
+export const createA2A = async (
+  agent: Agent,
+  params: {
+    store?: BaseStore;
+    model?: BaseChatModel;
+    messageEvent?: AgentMessageEvent;
+    chatOptions?: ChatOptions;
+    signal?: AbortSignal;
+    configurable?: Record<string, any>;
+  },
+) => {
+  const client = new A2AClient(agent.remote_url, fetch);
+  const agentCard = await new A2ACardResolver(agent.remote_url).getAgentCard();
+  const chain = new A2AChain({
+    client,
+    agentCard,
+    messageEvent: params.messageEvent,
+  });
+
+  return chain;
 };
