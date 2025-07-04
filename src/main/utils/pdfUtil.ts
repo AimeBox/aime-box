b56879e6581c2ebd1a50e7596aca56a8ffe256b8
noah <781172480@qq.com>
2025-07-04 12:00:08 +0800

    修改知识库显示错误

--- src/main/utils/pdfUtil.ts
+++ src/main/utils/pdfUtil.ts
@@ -4,6 +4,7 @@ import { getTmpPath, getAssetPath } from './path';
 import { spawn } from 'child_process';
 import { runCommand } from './exec';
 import fs from 'fs';
+import { dialog } from 'electron';
 
 /**
  * 将 PDF 每一页渲染为 PNG 图片
@@ -13,19 +14,44 @@ import fs from 'fs';
 export async function convertPdfToImages(pdfPath: string, outputDir: string) {
   // const info_data = await info(pdfPath);
   // console.log(info_data);
-  const popplerPath = path.join(
-    getAssetPath(),
-    'libs',
-    'poppler',
-    'pdftoppm.exe',
-  );
+  if (process.platform === 'win32') {
+    const popplerPath = path.join(
+      getAssetPath(),
+      'libs',
+      'poppler',
+      'pdftoppm.exe',
+    );
 
-  const tmpDirPrefix = path.join(outputDir, 'pdf-convert-');
-  const tmpDir = fs.mkdtempSync(tmpDirPrefix);
-  const outputPrefix = path.join(tmpDir, 'page');
-  const res = await runCommand(
-    `"${popplerPath}" "${pdfPath}" -jpeg "${outputPrefix}"`,
-  );
-  const files = await fs.promises.readdir(tmpDir);
-  return files.map((file) => path.join(tmpDir, file));
+    const tmpDirPrefix = path.join(outputDir, 'pdf-convert-');
+    const tmpDir = fs.mkdtempSync(tmpDirPrefix);
+    const outputPrefix = path.join(tmpDir, 'page');
+    const res = await runCommand(
+      `"${popplerPath}" "${pdfPath}" -jpeg "${outputPrefix}"`,
+    );
+    const files = await fs.promises.readdir(tmpDir);
+    return files.map((file) => path.join(tmpDir, file));
+  } else if (process.platform === 'darwin') {
+    let hasPoppler = false;
+    try {
+      const res = await runCommand(`poppler -v`);
+      hasPoppler = true;
+    } catch {}
+    if (!hasPoppler) {
+      const res = await dialog.showMessageBox({
+        title: 'Poppler not found',
+        message: 'Poppler not found, please install it',
+        buttons: ['OK', 'Cancel'],
+      });
+      if (res.response === 1) {
+        throw new Error('Poppler install cancelled');
+      }
+      await runCommand(`brew install poppler`);
+      await runCommand(`poppler -v`);
+    }
+
+    return [];
+    //brew install poppler
+  } else {
+    throw new Error('not supported');
+  }
 }
